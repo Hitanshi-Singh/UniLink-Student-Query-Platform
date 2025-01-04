e90ae9676922d27f8c32924b2af25f3b8f563eb4
nida242004 <syedanidafathima1@gmail.com>
2025-01-04 23:37:23 +0530

    questions history page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/pages/login.jsx'
 import Tags from './components/pages/Tags.jsx'
+import Questions from './components/pages/questions.jsx'
 import Signup from './components/pages/signup.jsx'
 function App() {
   
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       
-      <Tags/>
+      <Questions/>
     </>
   )
 }
